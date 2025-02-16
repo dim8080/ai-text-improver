bf1792950acb28ef8ff4a0221a741ad05c449edb
preslavdimitrov <preslav.dimitrov@ecollect.net>
2025-02-17 01:57:24 +0200

    Benefit from the .env

--- src/config/rateLimit.ts
+++ src/config/rateLimit.ts
@@ -1,6 +1,6 @@
 import { Options } from 'express-rate-limit'
 
 export const RateLimitConfig: Partial<Options> = {
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // 100 requests per 15 minutes
+  windowMs: process.env.RATE_LIMIT_WINDOW_MS ? parseInt(process.env.RATE_LIMIT_WINDOW_MS) : 15 * 60 * 1000, // Default 15 minutes
+  max: process.env.RATE_LIMIT_MAX ? parseInt(process.env.RATE_LIMIT_MAX) : 100 // Default  100 requests per 15 minutes
 };
